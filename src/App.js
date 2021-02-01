f1cb1da27907973897bc2fa69cf88537e423a90c
Goncalo <goncalo-m-ferreira@telecom.pt>
2021-02-01 18:04:52 +0000

    Change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Made by Gonçalo!
+          --Enjoy the App--
+          [Made by Gonçalo]
         </p>
         <a
           className="App-link"
